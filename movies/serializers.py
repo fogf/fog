ccc28b64a86af68c6ee280e6f6e45d68e0afb2a6
latyas <latyas@latyasdeMacBook-Pro.local>
2017-03-01 17:32:38 +0800

    add maybe restful api?

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,21 @@
 from .models import *
 from rest_framework import serializers
 
+class CelebritySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Celebrity
+        fields = '__all__'
 
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
+
+    directors = CelebritySerializer(many=True, read_only=True)
+    scriptwriters = CelebritySerializer(many=True, read_only=True)
     class Meta:
         model = Movie
         fields = '__all__'
+
+class CelebritySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Celebrity
+        fields = '__all__'
+
